2d65f44102f778c0c4e9dd33f4753e1a1db71e19
Kristen Florey <64163075+kristenflorey@users.noreply.github.com>
2021-03-23 10:01:37 -0400

    add nav

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,35 +8,41 @@ import sunrise_balloon from "../../img/sunrise-balloon.png";
 import striped_balloon from "../../img/striped-balloon.png";
 import right_cloud from "../../img/right-cloud.png";
 import left_cloud from "../../img/left-cloud.png";
+import desert_landscape from "../../img/desert-sun.png";
 
 const Home = () => {
     return (
             <div className="main-home">
-                <img className="home-logo" alt="home-logo" src={sun_logo}/>
-                <img className="home-landscape" alt="home-landscape" src={landscape}/>
-                <img className="striped-balloon" alt="striped-balloon" src={striped_balloon}/>
-                <img className="sunrise-balloon" alt="sunrise-balloon" src={sunrise_balloon}/>
-                <img className="right-cloud" alt="right-cloud" src={right_cloud}/>
-                <img className="left-cloud" alt="left-cloud" src={left_cloud}/>
+                <Navigation />
+                <div className="home-illustration-1">
+                    {/* <img className="home-logo" alt="home-logo" src={sun_logo}/>
+                    <img className="home-landscape" alt="home-landscape" src={landscape}/> */}
+                    <img className="striped-balloon" alt="striped-balloon" src={striped_balloon}/>
+                    <img className="sunrise-balloon" alt="sunrise-balloon" src={sunrise_balloon}/>
+                    {/* <img className="right-cloud" alt="right-cloud" src={right_cloud}/>
+                    <img className="left-cloud" alt="left-cloud" src={left_cloud}/> */}
 
-                <div class="container">
-                    <div class="bird-container--one">
-                        <div class="bird--one"></div>
-                    </div>
+                    <div class="container">
+                        <div class="bird-container--one">
+                            <div class="bird--one"></div>
+                        </div>
 
-                    <div class="bird-container--two">
-                        <div class="bird--two"></div>
-                    </div>
+                        <div class="bird-container--two">
+                            <div class="bird--two"></div>
+                        </div>
 
-                    <div class="bird-container--three">
-                        <div class="bird--three"></div>
-                    </div>
+                        <div class="bird-container--three">
+                            <div class="bird--three"></div>
+                        </div>
 
-                    <div class="bird-container--four">
-                        <div class="bird--four"></div>
+                        <div class="bird-container--four">
+                            <div class="bird--four"></div>
+                        </div>
                     </div>
                 </div>
-
+                <div className="home-illustration-2">
+                {/* <img className="desert-landscape" alt="desert-landscape" src={desert_landscape} /> */}
+                </div>
 
             </div>
     )
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,12 +6,21 @@ import SiteMap from '../SiteMap'
 const Navigation = () => {
     return (
         <nav>
-            <div>
-                <NavLink to="/" className="home-button">
-                    <img className="home-logo" alt="home-logo"/>
-                </NavLink>
-                <SiteMap />
-            </div>
+            {/* <NavLink to="/" className="home-button">
+                <img className="home-logo" alt="home-logo"/>
+            </NavLink> */}
+            <NavLink to="/projects" className="projects-link">
+                Projects
+            </NavLink>
+            <NavLink to="/wireframes" className="wireframes-link">
+                Wireframes
+            </NavLink>
+            <NavLink to="/paintings" className="paintings-link">
+                Paintings
+            </NavLink>
+            <NavLink to="/contact" className="contact-link">
+                Contact
+            </NavLink>
         </nav>
     )
 }
